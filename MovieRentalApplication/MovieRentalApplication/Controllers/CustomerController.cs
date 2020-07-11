4c9db5f79fb9cf1312e211b85063e15428f6dfd4
Mushfiqur Rahman <mushfiqurrahman749@gmail.com>
2020-07-11 20:43:08 +0600

    Customer controller, model, view added, NavBar changed

---
+++ MovieRentalApplication/MovieRentalApplication/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using MovieRentalApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRentalApplication.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+			List<Customer> Customers = new List<Customer> {
+				new Customer { id=1, Name="Customer 1"},
+				new Customer{id=2,Name="Customer 2"}
+
+			};
+
+            return View(Customers);
+        }
+    }
+}
