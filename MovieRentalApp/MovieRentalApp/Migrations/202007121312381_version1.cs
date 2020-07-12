d78678620edf09a0b6e60f844ed49c86d9040b2f
Mushfiqur Rahman <mushfiqurrahman749@gmail.com>
2020-07-12 19:15:05 +0600

    1st version of database migrated

---
+++ MovieRentalApp/MovieRentalApp/Migrations/202007121312381_version1.cs
@@ -0,0 +1,36 @@
+﻿namespace MovieRentalApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class version1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        MovieName = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+            DropTable("dbo.Customers");
+        }
+    }
+}
---
+++ MovieRentalApp/MovieRentalApp/Models/ProjectDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MovieRentalApp.Models
+{
+	public class ProjectDbContext:DbContext
+	{
+		public DbSet<Customer>Customers { get; set; }
+		public DbSet<Movie> Movies { get; set; }
+	}
+}
