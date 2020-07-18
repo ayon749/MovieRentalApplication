f5f3ef51eb90fa9a9e024ac26a24a34dce129638
Mushfiqur Rahman <mushfiqurrahman749@gmail.com>
2020-07-18 20:34:37 +0600

    customer controller view of index and new edit done

---
+++ movierApp/movierApp/Controllers/CustomerController.cs
@@ -0,0 +1,75 @@
+﻿using movierApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+
+namespace movierApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+		private ApplicationDbContext db = new ApplicationDbContext();
+		// GET: Customer
+		public ActionResult Index()
+		{
+			var customer = db.Customers.Include(c=>c.MembershipType).ToList();
+			return View(customer);
+		}
+		//[Route("")]
+		public ActionResult Details(int id)
+		{
+			var customer = db.Customers.Include(c => c.MembershipType).SingleOrDefault(a => a.id == id);
+			if (customer == null)
+			{
+				return HttpNotFound();
+			}
+			else
+			{
+				return View(customer);
+			}
+		}
+		public ActionResult New()
+		{
+			ViewBag.MembershipTypeId = new SelectList(db.MembershipTypes, "id", "Name");
+			return View();
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Save([Bind(Exclude = "Id")]Customer customer)
+		{
+			var errors = ModelState.Values.SelectMany(v => v.Errors);
+			if (!ModelState.IsValid)
+			{
+				ViewBag.MembershipTypeId = new SelectList(db.MembershipTypes, "id", "Name");
+				return View("New", customer);
+
+			}
+			else
+			{
+				if (customer.id == 0)
+				{
+					db.Customers.Add(customer);
+				}
+				else
+				{
+					var aCustomer = db.Customers.FirstOrDefault(c => c.id == customer.id);
+					aCustomer.Name = customer.Name;
+					aCustomer.BirthDate = customer.BirthDate;
+					aCustomer.MembershipTypeId = customer.MembershipTypeId;
+					aCustomer.IsSubscribeToNewsLetter = customer.IsSubscribeToNewsLetter;
+				}
+				db.SaveChanges();
+				return RedirectToAction("Index", "Customer");
+			}
+		}
+		public ActionResult Edit(int id)
+		{
+			var customer = db.Customers.FirstOrDefault(a => a.id == id);
+			ViewBag.MembershipTypeId = new SelectList(db.MembershipTypes, "id", "Name");
+			return View("New", customer);
+
+		}
+	}
+}
